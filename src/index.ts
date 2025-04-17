dd7a3f01b085f5f839186de04c97fc9c883c476f
rane254 <mr.appmaker.dev@gmail.com>
2025-04-17 23:24:33 +0530

    created a express - node server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send({ status: 200, message: "Welcome to my node server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT}`);
+});
